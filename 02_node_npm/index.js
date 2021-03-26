e0b1f8cac994d425dbaec060d8c9689ff1aadce5
Daniel Schwensen <daniel.schwensen@gmail.com>
2021-03-26 12:24:58 +0100

    Read and write the asynchronous way 2

--- 02_node_npm/index.js
+++ 02_node_npm/index.js
@@ -8,7 +8,26 @@ fs.writeFileSync('./txt/output.txt', textOut);
 console.log('File has been written');
 
 // Non-blocking, asynchronous way
+// 1.
 fs.readFile('./txt/start.txt', 'utf-8', (err, data) => {
     console.log(data);
+});
+console.log('Reading file');
+
+//--------------------
+console.log('----------------------');
+
+// 2.
+fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+    fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+        console.log(data2);
+        fs.readFile('./txt/append.txt', 'utf-8', (err, data3) => {
+            console.log(data3);
+        
+            fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
+                console.log('File has been written');
+            })
+        });
+    });
 });
 console.log('Reading file');
